5fb4ac1bdb0ccf2f10b2e0df5358cef955deb360
Cole Reynolds <cpjreynolds@gmail.com>
2015-07-26 05:27:25 +0000

    rebuild pages at addc504

--- implementors/core/ops/trait.Index.js
+++ implementors/core/ops/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustty'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='rustty/struct.Terminal.html' title='rustty::Terminal'>Terminal</a>",];
+implementors['rustty'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='rustty/struct.Terminal.html' title='rustty::Terminal'>Terminal</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>, <a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>)</a>&gt; for <a class='struct' href='rustty/struct.Terminal.html' title='rustty::Terminal'>Terminal</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
