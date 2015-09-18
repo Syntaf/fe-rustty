6f83a2a911cec6a94461ea183e0e9ea6181d538e
Cole Reynolds <cpjreynolds@gmail.com>
2015-09-18 00:53:50 +0000

    rebuild pages at a8d89e2

--- implementors/term/trait.Terminal.js
+++ implementors/term/trait.Terminal.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['term'] = [];implementors['rustty'] = [];
+implementors['term'] = [];implementors['rustty'] = [];implementors['rustty'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
